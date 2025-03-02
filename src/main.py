ae8a0187e15ac8a007f17eb0bbb9b1ed83b2c60e
rafablog77 <143825964+rafablog77@users.noreply.github.com>
2025-03-02 17:52:14 -0300

    Add greeting window

--- src/main.py
+++ src/main.py
@@ -1,35 +1,11 @@
 from PySide6 import QtCore, QtWidgets, QtGui
 from PySide6.QtCore import Qt
+
 import pandas as pd
 
-from pathlib import Path
+import os
 
-def pathwrap(path):
-    return str(Path(path))
-
-class TableModel(QtCore.QAbstractTableModel):
-    def __init__(self, data):
-        super().__init__()
-        self._data = data
-
-    def data(self, index, role):
-        if role == Qt.DisplayRole:
-            value = self._data.iloc[index.row()][index.column()]
-            return str(value)
-
-    def rowCount(self, index):
-        return self._data.shape[0]
-
-    def columnCount(self, index):
-        return self._data.shape[1]
-    
-    def headerData(self, section, orientation, role):
-        if role == Qt.DisplayRole:
-            if orientation == Qt.Horizontal:
-                return str(self._data.columns[section])
-            elif orientation == Qt.Vertical:
-                return str(self._data.index[section])
-
+from utils import pathwrap, TableModel
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -42,43 +18,63 @@ class MainWindow(QtWidgets.QMainWindow):
         self.addToolBar(toolbar)
 
         # Import and Export buttons
-        import_action = QtGui.QAction(QtGui.QIcon(pathwrap("./icons/document-import.png")), "Import", self)
+        import_action = QtGui.QAction(
+            QtGui.QIcon(pathwrap("./icons/document-import.png")), "Import", self
+        )
         import_action.setStatusTip("Imports cars from a file.")
 
-        export_action = QtGui.QAction(QtGui.QIcon(pathwrap("./icons/document-export.png")), "Export", self)
+        export_action = QtGui.QAction(
+            QtGui.QIcon(pathwrap("./icons/document-export.png")), "Export", self
+        )
         export_action.setStatusTip("Exports the open collection to a file.")
 
         # Undo and Redo buttons
-        undo_action = QtGui.QAction(QtGui.QIcon(pathwrap("./icons/arrow-curve-180.png")), "Undo", self)
+        undo_action = QtGui.QAction(
+            QtGui.QIcon(pathwrap("./icons/arrow-curve-180.png")), "Undo", self
+        )
         undo_action.setStatusTip("Undoes the latest action.")
         undo_action.setShortcut(QtGui.QKeySequence("Ctrl+z"))
 
-        redo_action = QtGui.QAction(QtGui.QIcon(pathwrap("./icons/arrow-curve.png")), "Redo", self)
+        redo_action = QtGui.QAction(
+            QtGui.QIcon(pathwrap("./icons/arrow-curve.png")), "Redo", self
+        )
         redo_action.setStatusTip("Redoes the latest undo.")
         redo_action.setShortcut(QtGui.QKeySequence("Ctrl+y"))
 
         # Clipboard buttons
-        cut_action = QtGui.QAction(QtGui.QIcon(pathwrap("./icons/scissors.png")), "Cut", self)
-        cut_action.setStatusTip("Copies the selected car to the clipboard and removes it.")
+        cut_action = QtGui.QAction(
+            QtGui.QIcon(pathwrap("./icons/scissors.png")), "Cut", self
+        )
+        cut_action.setStatusTip(
+            "Copies the selected car to the clipboard and removes it."
+        )
         cut_action.setShortcut(QtGui.QKeySequence("Ctrl+x"))
 
-        copy_action = QtGui.QAction(QtGui.QIcon(pathwrap("./icons/document-copy.png")), "Copy", self)
+        copy_action = QtGui.QAction(
+            QtGui.QIcon(pathwrap("./icons/document-copy.png")), "Copy", self
+        )
         copy_action.setStatusTip("Copies the selected car to the clipboard.")
         copy_action.setShortcut(QtGui.QKeySequence("Ctrl+c"))
 
-        paste_action = QtGui.QAction(QtGui.QIcon(pathwrap("./icons/clipboard.png")), "Paste", self)
+        paste_action = QtGui.QAction(
+            QtGui.QIcon(pathwrap("./icons/clipboard.png")), "Paste", self
+        )
         paste_action.setStatusTip("Pastes a car from the clipboard.")
         paste_action.setShortcut(QtGui.QKeySequence("Ctrl+c"))
 
         # Car buttons
-        add_action = QtGui.QAction(QtGui.QIcon(pathwrap("./icons/car--plus.png")), "Add Cars", self)
+        add_action = QtGui.QAction(
+            QtGui.QIcon(pathwrap("./icons/car--plus.png")), "Add Cars", self
+        )
         add_action.setStatusTip("Adds a car to the collection.")
         add_action.setShortcut(QtGui.QKeySequence("Ins"))
 
-        remove_action = QtGui.QAction(QtGui.QIcon(pathwrap("./icons/car--minus.png")), "Remove Cars", self)
+        remove_action = QtGui.QAction(
+            QtGui.QIcon(pathwrap("./icons/car--minus.png")), "Remove Cars", self
+        )
         remove_action.setStatusTip("Removes a car from the collection.")
         remove_action.setShortcut(QtGui.QKeySequence("Del"))
-        
+
         toolbar.addAction(import_action)
         toolbar.addAction(export_action)
 
@@ -101,16 +97,73 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.table = QtWidgets.QTableView()
 
-        data = pd.DataFrame([
-            [1,2,3],
-            [23,46,123],
-        ], columns=["a","b","c"], index=["1","2"])
+        data = pd.DataFrame(
+            [],
+            columns=["Image", "Model Name", "Toy Number", "Series", "Color", "Price"],
+            index=[],
+        )
 
         self.model = TableModel(data)
         self.table.setModel(self.model)
 
-        mainlayout = QtWidgets.QHBoxLayout()
-        sidelayout = QtWidgets.QVBoxLayout()
+        if not os.path.exists(pathwrap("./config/collections.json")):
+            dialog = GreetingWindow()
+            dialog.setWindowTitle("Welcome!")
+            dialog.exec()
+
+        main_layout = QtWidgets.QHBoxLayout()
+        side_layout = QtWidgets.QVBoxLayout()
+        image_layout = QtWidgets.QVBoxLayout()
+        info_layout = QtWidgets.QVBoxLayout()
+
+
+class GreetingWindow(QtWidgets.QDialog):
+    def __init__(self):
+        super().__init__()
+
+        self.setFixedSize(QtCore.QSize(600, 350))
+
+        banner = QtWidgets.QLabel(self)
+        pixmap = QtGui.QPixmap(pathwrap("./icons/wheelshelf_banner.png"))
+        banner.setPixmap(pixmap)
+
+        title = QtWidgets.QLabel("Welcome to WheelShelf!")
+        title_font = title.font()
+        title_font.setPointSize(20)
+        title.setFont(title_font)
+        title.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+
+        desc = QtWidgets.QLabel("WheelShelf is a powerful Hot Wheels\ncollection management software for PC")
+        desc_font = desc.font()
+        desc_font.setPointSize(14)
+        desc.setFont(desc_font)
+
+        help = QtWidgets.QLabel("Create or open a existent collection")
+        help_font = help.font()
+        help_font.setPointSize(10)
+        help.setFont(help_font)
+        help.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+
+        new_btn = QtWidgets.QPushButton("New Collection")
+
+        import_btn = QtWidgets.QPushButton("Import Collection file")
+
+        open_btn = QtWidgets.QPushButton("Open collection from folder")
+
+        buttons = QtWidgets.QVBoxLayout()
+        buttons.addWidget(new_btn)
+        buttons.addWidget(import_btn)
+        buttons.addWidget(open_btn)
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(banner, alignment=Qt.AlignmentFlag.AlignTop)
+        layout.addWidget(title)
+        layout.addSpacing(10)
+        layout.addWidget(desc)
+        layout.addSpacing(10)
+        layout.addWidget(help)
+        layout.addLayout(buttons)
+        self.setLayout(layout)
 
 app = QtWidgets.QApplication([])
 window = MainWindow()
---
+++ src/utils.py
@@ -0,0 +1,30 @@
+from PySide6 import QtCore
+from PySide6.QtCore import Qt
+
+from pathlib import Path
+
+def pathwrap(path):
+    return str(Path(path))
+
+class TableModel(QtCore.QAbstractTableModel):
+    def __init__(self, data):
+        super().__init__()
+        self._data = data
+
+    def data(self, index, role):
+        if role == Qt.DisplayRole:
+            value = self._data.iloc[index.row()][index.column()]
+            return str(value)
+
+    def rowCount(self, index):
+        return self._data.shape[0]
+
+    def columnCount(self, index):
+        return self._data.shape[1]
+
+    def headerData(self, section, orientation, role):
+        if role == Qt.DisplayRole:
+            if orientation == Qt.Horizontal:
+                return str(self._data.columns[section])
+            elif orientation == Qt.Vertical:
+                return str(self._data.index[section])
