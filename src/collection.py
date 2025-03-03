a97c42aae054319a5fae1c305b686edb7592236d
rafablog77 <143825964+rafablog77@users.noreply.github.com>
2025-03-03 11:31:07 -0300

    Add basic collection

--- src/collection.py
+++ src/collection.py
@@ -3,6 +3,13 @@ from PySide6.QtCore import Qt
 
 from utils import pathwrap, ButtonLineEdit
 
+from pathlib import Path
+import sqlite3
+import shutil
+import json
+import os
+
+
 class NewCollection(QtWidgets.QDialog):
     def __init__(self):
         super().__init__()
@@ -15,20 +22,21 @@ class NewCollection(QtWidgets.QDialog):
 
         desc = QtWidgets.QLabel("A collection is where all your cars are stored.")
 
-        name_input = QtWidgets.QLineEdit()
-        name_input.setPlaceholderText("The name of your collection")
+        self.name_input = QtWidgets.QLineEdit()
+        self.name_input.setPlaceholderText("The name of your collection")
 
         self.path_input = ButtonLineEdit(pathwrap("./icons/folder--arrow.png"))
         self.path_input.setPlaceholderText("The path for your collection")
         self.path_input.buttonClicked.connect(self.selectPath)
 
         form = QtWidgets.QFormLayout()
-        form.addRow("Name:", name_input)
+        form.addRow("Name:", self.name_input)
         form.addRow("Path:", self.path_input)
 
         buttons = QtWidgets.QDialogButtonBox.Ok | QtWidgets.QDialogButtonBox.Cancel
         button_box = QtWidgets.QDialogButtonBox(buttons)
         button_box.rejected.connect(self.reject)
+        button_box.accepted.connect(self.createCollection)
 
         layout = QtWidgets.QVBoxLayout()
         layout.addWidget(title)
@@ -40,5 +48,47 @@ class NewCollection(QtWidgets.QDialog):
         self.setLayout(layout)
 
     def selectPath(self):
-        directory = QtWidgets.QFileDialog.Get
+        directory = QtWidgets.QFileDialog.getExistingDirectory()
         self.path_input.setText(directory)
+
+    def createCollection(self):
+        path = str(Path(self.path_input.text(), self.name_input.text()))
+        dialog = QtWidgets.QMessageBox.question(
+            self,
+            "Confirm Collection",
+            "Collection will be created at\n%s" % path,
+            buttons=QtWidgets.QMessageBox.Ok | QtWidgets.QMessageBox.Cancel,
+            defaultButton=QtWidgets.QMessageBox.Cancel,
+        )
+        if dialog == QtWidgets.QMessageBox.Ok:
+
+            Path(path).mkdir(parents=True)
+            if not os.path.exists(path):
+                QtWidgets.QMessageBox.critical(
+                    self, "Failed", "Failed to create Collection"
+                )
+
+            conn = sqlite3.connect(Path(path, "collection.db"))
+            conn.close()
+            if not os.path.exists(Path(path, "collection.db")):
+                QtWidgets.QMessageBox.critical(
+                    self, "Failed", "Failed to create Collection"
+                )
+
+            data = {"name": self.name_input.text()}
+
+            with open(Path(path, "info.json"), "w") as f:
+                f.write(json.dumps(data))
+
+            if not os.path.exists(Path(path, "info.json")):
+                QtWidgets.QMessageBox.critical(
+                    self, "Failed", "Failed to create Collection"
+                )
+
+            done = QtWidgets.QMessageBox.information(
+                self,
+                "Done!",
+                "Finished creating collection.",
+                buttons=QtWidgets.QMessageBox.Ok
+            )
+            
